1adcf5dfa1cdcda913512b7fa0fa52cd6436f705
Saurabh <sensaurabh1212@gmail.com>
2023-10-10 12:59:07 +0530

    home page UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,26 @@ import './App.scss';
 import logo from "./assets/zevi-logo.png";
 import Search from './components/Search/Search';
 import SearchSuggestions from './components/SearchSuggestions/SearchSuggestions';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from './GlobalState/store';
+import { setShowSuggestions } from './Features/Search/SearchSlice';
 
 function App() {
 
+  const dispatch = useDispatch();
+
   const showSuggestions = useSelector((state: RootState) => state.search.showSuggestions)
 
+  // close suggestions when user clicks outside of suggestions
+  const handleCloseSuggestions = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const target = e.target as HTMLDivElement;
+    if (target.classList.contains('App')) {
+      dispatch(setShowSuggestions(false))
+    }
+  }
+
   return (
-    <div className="App">
+    <div className="App" onClick={handleCloseSuggestions}>
       <header className="nav">
         <img src={logo} alt="zevi dot ai logo" />
       </header>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import './index.scss';
 import App from './App';
+import SearchResults from './Routes/SearchResults/SearchResults';
 import { store } from './GlobalState/store'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/search",
+    element: <SearchResults />,
+  }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +28,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
